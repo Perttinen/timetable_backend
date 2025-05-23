784c2065cf4658c39b141e8575e70b3e65ee30a6
perttinen <samuli.viitala@gmail.com>
2025-05-23 19:06:52 +0300

    Prettier format in scripts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 
 const app = express();
 
-const port = process.env.PORT ?? '3001'
+const port = process.env.PORT ?? "3001";
 
 app.get("/", (req, res) => {
-    res.send("Hello Worl!");
-    console.log("Response sent");
+  res.send("Hello Worl!");
+  console.log("Response sent");
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
