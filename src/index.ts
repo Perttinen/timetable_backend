339b80b5064b3532d4b7e7bd169751cebf6e9ead
perttinen <samuli.viitala@gmail.com>
2025-05-25 10:11:54 +0300

    test commit

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,25 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 
+import Test from "./models/test.js";
+
 const app = express();
 
 const port = process.env.PORT ?? "3001";
+
+const dbTest = async () => {
+  const testthing = new Test({
+    testString: "Just testing4",
+  });
+  const result = await testthing.save();
+  console.log(result);
+  const deleted = await Test.deleteMany({});
+  console.log(deleted);
+};
+
+dbTest().catch((err: unknown) => {
+  console.log(err instanceof Error ? err.message : "Unexpected error");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Worl!");
